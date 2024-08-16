5a5e3e4f76fe5829fda26978071134e3f3bf9aa6
andresignaciodeagustini <104034821+andresignaciodeagustini@users.noreply.github.com>
2024-08-16 17:43:05 -0300

    Update order.routes.js

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -8,4 +8,11 @@ router.post("/orders", auth, orderController.postOrder);
 // Obtener todas las órdenes, opcionalmente filtrando por idUser
 router.get("/orders/:idUser?", auth, orderController.getOrders);
 
+// Obtener una orden específica por ID
+router.get("/orders/:orderId", auth, orderController.getOrderById);
+
+// Eliminar una orden específica por ID
+router.delete("/orders/:orderId", auth, orderController.deleteOrder);
+
+
 module.exports = router;
